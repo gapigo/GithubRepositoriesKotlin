f6b6c741fc1aace41d93ebbea2e680f7df671639
gapigo <x.gabriel12@gmail.com>
2022-08-07 23:24:25 -0300

    Criação do menu de busca

--- app/src/main/java/com/gapigo/repositories/ui/MainActivity.kt
+++ app/src/main/java/com/gapigo/repositories/ui/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.gapigo.repositories.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.gapigo.repositories.R
 import com.gapigo.repositories.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
 
@@ -14,5 +17,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu?.findItem(R.id.action_search)?.actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.e(TAG, "onQueryTextSubmit: $query")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $newText")
+        return true
+    }
+
+    companion object {
+        private const val TAG = "TAG"
     }
 }
