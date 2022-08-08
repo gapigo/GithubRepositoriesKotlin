709f366a62333a20eb4ac302e5cafed029b254de
gapigo <x.gabriel12@gmail.com>
2022-08-07 23:08:48 -0300

    Criando o item da listagem

--- app/src/main/java/com/gapigo/repositories/ui/MainActivity.kt
+++ app/src/main/java/com/gapigo/repositories/ui/MainActivity.kt
@@ -3,10 +3,16 @@ package com.gapigo.repositories.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gapigo.repositories.R
+import com.gapigo.repositories.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
     }
 }
