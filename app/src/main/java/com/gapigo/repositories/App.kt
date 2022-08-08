ad9c8f9b1e2f3538125891c138ceee051cde3118
gapigo <x.gabriel12@gmail.com>
2022-08-07 23:38:01 -0300

    Criação do Data Module

--- app/src/main/java/com/gapigo/repositories/App.kt
+++ app/src/main/java/com/gapigo/repositories/App.kt
@@ -1,6 +1,7 @@
 package com.gapigo.repositories
 
 import android.app.Application
+import com.gapigo.repositories.data.di.DataModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -12,5 +13,7 @@ class App : Application() {
         startKoin {
             androidContext(this@App)
         }
+
+        DataModule.load()
     }
 }
---
+++ app/src/main/java/com/gapigo/repositories/data/services/GitHubService.kt
@@ -0,0 +1,10 @@
+package com.gapigo.repositories.data.services
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitHubService {
+ @GET("users/{user}/repos")
+ suspend fun listRepositories(@Path("user") user: String): List<Repo>
+}
